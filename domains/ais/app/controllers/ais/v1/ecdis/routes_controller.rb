3d9de3ab471c9e14e42567d8fd2977cb7f8ab438
lnnah <nam.le@dounets.com>
2021-04-05 15:36:12 +0700

    feedback/GNG-1948-Show-ecdis-info-in-bottom-panel

--- domains/ais/app/controllers/ais/v1/ecdis/routes_controller.rb
+++ domains/ais/app/controllers/ais/v1/ecdis/routes_controller.rb
@@ -9,7 +9,7 @@ module Ais
           routes = Ais::EcdisRoute
                     .imo(filter_params[:imo])
                     .of_month(filter_params[:time]&.to_datetime)
-                    .order(:created_at)
+                    .order(received_at: :desc)
           routes_json = Ais::V1::Ecdis::RoutesSerializer.new(routes).serializable_hash
           json_response(routes_json)
         end
--- domains/ais/app/services/ais/utility_services/lat_lon_converter.rb
+++ domains/ais/app/services/ais/utility_services/lat_lon_converter.rb
@@ -6,7 +6,7 @@ module Ais
         return nil unless dds.present? && dds.length == 3
         
         dd = dds[0].to_f + dds[1].to_f / 60
-        return dd unless dds[2] != 'S' && dds[2] != 'W'
+        return dd unless dds[2].eql?('S') || dds[2].eql?('W')
 
         -dd
       end
@@ -15,7 +15,7 @@ module Ais
         return nil unless d.present? && m.present? && h.present?
 
         dd = d.to_f + m.to_f / 60
-        return dd unless h != 'S' && h != 'W'
+        return dd unless h.eql?('S') || h.eql?('W')
 
         -dd
       end

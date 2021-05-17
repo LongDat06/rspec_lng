4a734568217d4b234f72b529870033b64156d15c
Nghia <comictvn@gmail.com>
2021-05-17 13:18:11 +0700

    CD-1 exclude port panama & suez (#86)
    * CD-1 exclude port panama & suez
    
    * port code

--- domains/analytic/app/services/analytic/spas_services/last_departure.rb
+++ domains/analytic/app/services/analytic/spas_services/last_departure.rb
@@ -3,6 +3,7 @@ module Analytic
     class LastDeparture
       include Wisper::Publisher 
 
+      EXCLUDE_PORT_LOCODE = 'PABLB;PACTB;EGSCA;EGSUZ'.freeze
       DEPATURE_FLAG = '3:DEP'.freeze
 
       def initialize(imo)
@@ -11,7 +12,8 @@ module Analytic
 
       def call
         return if last_dep.blank?
-        last_dep.spec.attributes
+        port_code = last_dep.spec.attributes[:jsmea_voy_portinformation_portcode]
+        return if port_code.present? && EXCLUDE_PORT_LOCODE.include?(port_code.strip)
         broadcast(:on_dep_successful, { imo: @imo, spec: last_dep.spec.attributes })
       end
 

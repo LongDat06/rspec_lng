69405feecb34fb10d5b32dd681557c19ef6f26b2
thuydounets <53721851+thuydounets@users.noreply.github.com>
2022-01-27 14:07:15 +0700

    update for delete vessel (#301)

--- domains/analytic/app/models/concerns/clean_shipdc_data.rb
+++ domains/analytic/app/models/concerns/clean_shipdc_data.rb
@@ -1,8 +1,8 @@
 module CleanShipdcData
   def clear_old_related_sim_table(imo)
-    Analytic::Sim.where(imo_no: imo).destroy_all
-    Analytic::SimChannel.where(imo_no: imo).destroy_all
-    Analytic::SimMetadata.where(imo_no: imo).destroy_all
+    Analytic::Sim.where(imo_no: imo).delete_all
+    Analytic::SimChannel.where(imo_no: imo).delete_all
+    Analytic::SimMetadata.where(imo_no: imo).delete_all
   end
 end
 
